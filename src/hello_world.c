2c308b1dc56c19a23549ae90374720fc26bb3063
welazat <werner.lamprecht@zuehlke.com>
2017-06-20 08:56:58 +0000

    SIM-332 Changed output & comments

--- src/hello_world.c
+++ src/hello_world.c
@@ -1,12 +1,13 @@
 include<stdio.h>
 /**
  *
- *This is a Hello World C code example
+ * This is a bad Hello World C code example
+ * With no sense 
  *
  */
 int main() {
 	/* Prints "Hello World */
-	printf("Hello World\n");
+	printf("Hello World is bad \n");
 	return 0;
 }
 
